738a119181edeb47cd5cb1b7abba4c0e66481f76
ahergonb <ahergonb@gmail.com>
2025-05-05 15:42:09 -0600

    Create hello1.sh
    adding new script

---
+++ ch1/hello1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This is the firstr Bash shell
+# Scriptname : Hello1.sh
+# Written by: TobyHer
+echo "Hello $LOGNAME, Have a nice day!"
+echo "You are working in directory `pwd`."
+echo "You are working on a machine called `uname -o` ."
+echo "List of files in your directory is :"
+ls
+echo "Bye for now $LOGNAME. the time is `date +%T`!"
